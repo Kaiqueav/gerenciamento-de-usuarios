0134dcd09bf798aac2717e827303e93acbe57660
Kaiqueav <kaiquecerq66@outlook.com>
2024-03-25 22:03:52 -0300

    feat: insert  jpa annotaions

--- src/main/java/com/example/apiclima/entity/Person.java
+++ src/main/java/com/example/apiclima/entity/Person.java
@@ -1,19 +1,41 @@
 package com.example.apiclima.entity;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 
+import org.hibernate.mapping.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+@Entity
 @NoArgsConstructor
 @Builder
 @AllArgsConstructor
 @Data
 public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
     private String firstName;
+
+    @Column(nullable = false)
     private String lastName;
+
+    @Column(nullable = false, unique = true)
     private int cpf;
+
     private LocalDate birthDay;
+    @OneToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
+    private ArrayList<Phone> phones;
 }
