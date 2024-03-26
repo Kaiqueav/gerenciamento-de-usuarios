3f1ccdc2bb02ebbc5eb29c31745f4a5abf193100
Kaiqueav <kaiquecerq66@outlook.com>
2024-03-25 21:38:23 -0300

    feat: insert jpa annotations

--- src/main/java/com/example/apiclima/entity/Phone.java
+++ src/main/java/com/example/apiclima/entity/Phone.java
@@ -1,18 +1,32 @@
 package com.example.apiclima.entity;
 import com.example.apiclima.enums.PhoneType;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
 @NoArgsConstructor
 @Builder
 @Data
 @AllArgsConstructor
 public class Phone {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private Long id;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private PhoneType type;
+    @Column(nullable = false)
     private String number;
 
 }
