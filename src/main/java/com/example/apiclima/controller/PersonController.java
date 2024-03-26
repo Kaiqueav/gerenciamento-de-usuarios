4926ce898f103da427d06ae4e43877fcf94d89a6
Kaiqueav <kaiquecerq66@outlook.com>
2024-03-25 22:45:08 -0300

    feat: create personController

---
+++ src/main/java/com/example/apiclima/controller/PersonController.java
@@ -0,0 +1,20 @@
+package com.example.apiclima.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/people")
+public class PersonController {
+
+    private PersonRepository personRepository;
+
+    @Autowired
+    public PersonController(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+   
+}
