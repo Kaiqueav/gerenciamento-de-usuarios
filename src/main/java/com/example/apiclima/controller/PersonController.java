d1488a54bfbd348d6d73662ec899761b5f7dff9c
Kaiqueav <kaiquecerq66@outlook.com>
2024-03-27 22:53:50 -0300

    refactor post method

--- src/main/java/com/example/apiclima/controller/PersonController.java
+++ src/main/java/com/example/apiclima/controller/PersonController.java
@@ -1,8 +1,7 @@
 package com.example.apiclima.controller;
 import com.example.apiclima.dto.MessageResponseDTO;
 import com.example.apiclima.entity.Person;
-import com.example.apiclima.repository.PersonRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.example.apiclima.service.PersonService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,22 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/people")
 public class PersonController {
 
-    private PersonRepository personRepository;
-
-    @Autowired
-    public PersonController(PersonRepository personRepository) {
-        this.personRepository = personRepository;
+    private PersonService personService;
+  
+    public PersonController(PersonService personService) {
+        this.personService = personService;
     }
 
-   
     @PostMapping
     public MessageResponseDTO createPerson(@RequestBody Person person){
-        Person savedPerson = personRepository.save(person);
-        return MessageResponseDTO
-            .builder()
-            .message("created person with id " + savedPerson.getId())
-            .build();
-    }
+        return personService.createPerson(person);
+    }   
 
    
 }
